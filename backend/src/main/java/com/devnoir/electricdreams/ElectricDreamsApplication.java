1816acdeb96782b0a4cc1570f254e25db18980f2
emelinedev84 <emelinedev84@gmail.com>
2025-03-23 14:53:29 -0300

    Project created

---
+++ backend/src/main/java/com/devnoir/electricdreams/ElectricDreamsApplication.java
@@ -0,0 +1,13 @@
+package com.devnoir.electricdreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricDreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricDreamsApplication.class, args);
+	}
+
+}
